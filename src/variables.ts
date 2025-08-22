5842522954b02af3bec2e4d377d3a3aafd00e086
Erkan <ErkanOzulu@users.noreply.github.com>
2025-08-22 17:35:31 +0300

    loops

---
+++ src/variables.ts
@@ -0,0 +1,7 @@
+// 1. Değişken Tanımlama (let, const, var)
+
+let name: string = "Ahmet";      // değişebilir
+const age: number = 25;          // sabit
+var country: string = "İstanbul";  // global scoped (tercih edilmez)
+console.log(name)
+
