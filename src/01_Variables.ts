aed579db21071496c2061cc17ee0e7c550327a34
Erkan <ErkanOzulu@users.noreply.github.com>
2025-08-22 21:55:36 +0300

    updated

--- src/01_Variables.ts
+++ src/01_Variables.ts
@@ -5,3 +5,29 @@ const age: number = 25;          // sabit
 var country: string = "İstanbul";  // global scoped (tercih edilmez)
 console.log(userName)
 
+// as – Type Assertion
+
+let data: unknown = "Merhaba";
+
+//console.log(data.length);      //  bilinmeyen tip 
+console.log((data as string).length); //  8  
+// as, derleyiciye "eminim, bu şu tipte" demektir. Ama dikkat: yanlış kullanırsan hata verir.
+
+// unknown vs any
+
+let x: any = 5;
+x = "Merhaba";  // her şey olur, hiçbir kontrol yok
+
+let y: unknown = 10;
+y = "Test";     // değer atanır ama...
+
+// console.log(y.length);    //  Hata: unknown tipte
+if (typeof y === "string") {
+  console.log(y.length);    // Güvenli
+}
+
+// never, bu fonksiyonun asla geri dönmeyeceğini gösterir. Genelde hata fırlatıldığında veya sonsuz döngüde kullanılır.
+function giveError (mesaj: string): never {
+    throw new Error(mesaj);
+  }
+  giveError("Dur");
